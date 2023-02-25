f5ffca760182450657a6e81d5453d6b6f440ad26
Alec Hannaford <alec322@gmail.com>
2023-02-24 16:53:00 -0800

    catshow comp hooked up

---
+++ src/Pages/CatShow.js
@@ -0,0 +1,5 @@
+function CatShow(props){
+    return <h1>CATSHOW COMP</h1>
+}
+
+export default CatShow
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,21 +1,30 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Link } from "react-router-dom"
+
 
 function Index(props){
     const data = useLoaderData()
     console.log(data)
+    
+    //Make alist of unique categories
+    let allCategories = []
+    data.forEach((element) => allCategories.push(element.category))
+    console.log(allCategories)
 
-    //let allCategories = []
-    // data.forEach((element) => allCategories.push(element.category))
-    // console.log(allCategories)
-    // function uniqueCategories(element, index, arr){
-    //     return arr.indexOf(element) === index
-    // }
-    // const unique = data.filter(uniqueCategories)
-    // console.log(unique)
+    function uniqueCategories(element, index, arr){
+         return arr.indexOf(element) === index
+     }
+    const unique = allCategories.filter(uniqueCategories)
     
     return (
     <>
         <h1>Index COMPONENT</h1>
+        {unique.map((element, index) => {
+            return(
+            <ul key = {index}>
+                <Link to = {`catshow/${element}`}>{element}</Link>
+            </ul>
+            )
+        })}
     </>
     )
 }
